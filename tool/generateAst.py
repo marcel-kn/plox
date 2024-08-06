f6970c625eab0a5aacb77646a62e8389da24810a
Marcel Knüdeler <marcel.knuedeler@gmx.de>
2024-08-06 11:43:10 +0200

    fixed script to work with python

--- tool/generateAst.py
+++ tool/generateAst.py
@@ -11,10 +11,13 @@ output_dir = sys.argv[1]
 def define_ast(output_dir: str, file_name: str, base_name: str, types: list[str]):
     path = output_dir + "/" + file_name + ".py"
 
-    file = open(path)
+    file = open(path, "w")
+
+    file.write("from ltoken import Token\n\n")
+
     file.write("class " + base_name + ":\n")
     file.write("\t")
-    file.write("pass\n\n")
+    file.write("pass\n")
 
     for type in types:
         class_name = type.split(":")[0].strip()
@@ -25,14 +28,24 @@ def define_ast(output_dir: str, file_name: str, base_name: str, types: list[str]
 
 
 def define_type(file: TextIOWrapper, base_name, class_name, field_list: str):
-    file.write("class " + class_name + "(" + base_name + "):\n")
+    file.write("\nclass " + class_name + "(" + base_name + "):\n")
     # Constructor
-    file.write("\tdef __init__(self, " + field_list + ")\n\t")
-    # Store parameters in fields
-    fields: list[str] = field_list.split(" ")
+    fields: list[str] = field_list.split(", ")
+    params_string = ""
     for field in fields:
         name = field.split(" ")[1]
-        file.write("self." + name + " = " + name + "\n")
+        type = field.split(" ")[0]
+        if not type == "Object":
+            params_string += name + ": " + type + ", "
+        else:
+            params_string += name + ", "
+    params_string = params_string[:-2]
+    file.write("\tdef __init__(self, " + params_string + "):\n")
+    # Store parameters in fields
+
+    for field in fields:
+        name = field.split(" ")[1]
+        file.write("\t\tself." + name + " = " + name + "\n")
 
 
 define_ast(output_dir, "lx_ast", "Expr",
